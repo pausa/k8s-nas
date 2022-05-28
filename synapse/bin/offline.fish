eee7befba13cae5b82b8b0d1de22ea41011af759
pausa <pausatep@gmail.com>
2022-05-28 16:36:43 +0000

    googlechat bridge and postgres

---
+++ synapse/bin/offline.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/env fish
+docker run -it --rm \
+    -v ~/.config/synapse:/data \
+    -e SYNAPSE_SERVER_NAME=madpausa.sytes.net \
+    -e SYNAPSE_REPORT_STATS=yes \
+    -e UID=1000 \
+    -e GID=1000 \
+    --entrypoint=bash \
+    matrixdotorg/synapse:latest
